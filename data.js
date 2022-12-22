dd93b68b5cc81e26b2387ae7a43a7c858a995653
FlepJack1 <76703121+FlepJack1@users.noreply.github.com>
2022-12-22 16:34:56 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 const data = [
-    {id: 0, img: "../assets/parking.svg", text: "Парковая зона" },
-    {id: 1, img: "../assets/sport.svg", text: "Спортивная зона" },
-    {id: 2, img: "..//assets/education.svg", text: "Зона знаний" },
-    {id: 3, img: "../assets/expo.svg", text: "ЭКСПО" },
-    {id: 4, img: "../assets/entertainment.svg", text: "Развлечения" },
-    {id: 5, img: "../assets/museum.svg", text: "Музеи" }
+    {id: 0, img: "./images/parking.svg", text: "Парковая зона" },
+    {id: 1, img: "/images/sport.svg", text: "Спортивная зона" },
+    {id: 2, img: "../images/education.svg", text: "Зона знаний" },
+    {id: 3, img: "./images/expo.svg", text: "ЭКСПО" },
+    {id: 4, img: "./images/entertainment.svg", text: "Развлечения" },
+    {id: 5, img: "./images/museum.svg", text: "Музеи" }
 ]
 
 export default data;
