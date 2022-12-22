ec119284f2d9a4c9a257a1ccc831667b34bf627a
FlepJack1 <76703121+FlepJack1@users.noreply.github.com>
2022-12-22 16:19:05 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const data = [
-    {id: 0, img: "./assets/parking.svg", text: "Парковая зона" },
-    {id: 1, img: "./assets/sport.svg", text: "Спортивная зона" },
-    {id: 2, img: "./assets/education.svg", text: "Зона знаний" },
+    {id: 0, img: "assets/parking.svg", text: "Парковая зона" },
+    {id: 1, img: "assets/sport.svg", text: "Спортивная зона" },
+    {id: 2, img: "/assets/education.svg", text: "Зона знаний" },
     {id: 3, img: "./assets/expo.svg", text: "ЭКСПО" },
     {id: 4, img: "./assets/entertainment.svg", text: "Развлечения" },
     {id: 5, img: "./assets/museum.svg", text: "Музеи" }
